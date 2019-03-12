050e4d8719b4f13816ecfc4a1b008811a5a6401a
Gerold55 <marleybeare56@gmail.com>
2019-03-11 21:18:02 -0400

    Add files via upload

---
+++ mods/stairs/init.lua
@@ -0,0 +1,581 @@
+-- Minetest 0.4 mod: stairs
+-- See README.txt for licensing and other information.
+
+
+-- Global namespace for functions
+
+stairs = {}
+
+
+-- Register aliases for new pine node names
+
+minetest.register_alias("stairs:stair_pinewood", "stairs:stair_pine_wood")
+minetest.register_alias("stairs:slab_pinewood", "stairs:slab_pine_wood")
+
+
+-- Get setting for replace ABM
+
+local replace = minetest.settings:get_bool("enable_stairs_replace_abm")
+
+local function rotate_and_place(itemstack, placer, pointed_thing)
+	local p0 = pointed_thing.under
+	local p1 = pointed_thing.above
+	local param2 = 0
+
+	if placer then
+		local placer_pos = placer:get_pos()
+		if placer_pos then
+			param2 = minetest.dir_to_facedir(vector.subtract(p1, placer_pos))
+		end
+
+		local finepos = minetest.pointed_thing_to_face_pos(placer, pointed_thing)
+		local fpos = finepos.y % 1
+
+		if p0.y - 1 == p1.y or (fpos > 0 and fpos < 0.5)
+				or (fpos < -0.5 and fpos > -0.999999999) then
+			param2 = param2 + 20
+			if param2 == 21 then
+				param2 = 23
+			elseif param2 == 23 then
+				param2 = 21
+			end
+		end
+	end
+	return minetest.item_place(itemstack, placer, pointed_thing, param2)
+end
+
+
+-- Register stair
+-- Node will be called stairs:stair_<subname>
+
+function stairs.register_stair(subname, recipeitem, groups, images, description,
+		sounds, worldaligntex)
+	-- Set backface culling and world-aligned textures
+	local stair_images = {}
+	for i, image in ipairs(images) do
+		if type(image) == "string" then
+			stair_images[i] = {
+				name = image,
+				backface_culling = true,
+			}
+			if worldaligntex then
+				stair_images[i].align_style = "world"
+			end
+		else
+			stair_images[i] = table.copy(image)
+			if stair_images[i].backface_culling == nil then
+				stair_images[i].backface_culling = true
+			end
+			if worldaligntex and stair_images[i].align_style == nil then
+				stair_images[i].align_style = "world"
+			end
+		end
+	end
+	local new_groups = table.copy(groups)
+	new_groups.stair = 1
+	minetest.register_node(":stairs:stair_" .. subname, {
+		description = description,
+		drawtype = "nodebox",
+		tiles = stair_images,
+		paramtype = "light",
+		paramtype2 = "facedir",
+		is_ground_content = false,
+		groups = new_groups,
+		sounds = sounds,
+		node_box = {
+			type = "fixed",
+			fixed = {
+				{-0.5, -0.5, -0.5, 0.5, 0.0, 0.5},
+				{-0.5, 0.0, 0.0, 0.5, 0.5, 0.5},
+			},
+		},
+		on_place = function(itemstack, placer, pointed_thing)
+			if pointed_thing.type ~= "node" then
+				return itemstack
+			end
+
+			return rotate_and_place(itemstack, placer, pointed_thing)
+		end,
+	})
+
+	-- for replace ABM
+	if replace then
+		minetest.register_node(":stairs:stair_" .. subname .. "upside_down", {
+			replace_name = "stairs:stair_" .. subname,
+			groups = {slabs_replace = 1},
+		})
+	end
+
+	if recipeitem then
+		-- Recipe matches appearence in inventory
+		minetest.register_craft({
+			output = 'stairs:stair_' .. subname .. ' 8',
+			recipe = {
+				{"", "", recipeitem},
+				{"", recipeitem, recipeitem},
+				{recipeitem, recipeitem, recipeitem},
+			},
+		})
+
+		-- Use stairs to craft full blocks again (1:1)
+		minetest.register_craft({
+			output = recipeitem .. ' 3',
+			recipe = {
+				{'stairs:stair_' .. subname, 'stairs:stair_' .. subname},
+				{'stairs:stair_' .. subname, 'stairs:stair_' .. subname},
+			},
+		})
+
+		-- Fuel
+		local baseburntime = minetest.get_craft_result({
+			method = "fuel",
+			width = 1,
+			items = {recipeitem}
+		}).time
+		if baseburntime > 0 then
+			minetest.register_craft({
+				type = "fuel",
+				recipe = 'stairs:stair_' .. subname,
+				burntime = math.floor(baseburntime * 0.75),
+			})
+		end
+	end
+end
+
+
+-- Register slab
+-- Node will be called stairs:slab_<subname>
+
+function stairs.register_slab(subname, recipeitem, groups, images, description,
+		sounds, worldaligntex)
+	-- Set world-aligned textures
+	local slab_images = {}
+	for i, image in ipairs(images) do
+		if type(image) == "string" then
+			slab_images[i] = {
+				name = image,
+			}
+			if worldaligntex then
+				slab_images[i].align_style = "world"
+			end
+		else
+			slab_images[i] = table.copy(image)
+			if worldaligntex and image.align_style == nil then
+				slab_images[i].align_style = "world"
+			end
+		end
+	end
+	local new_groups = table.copy(groups)
+	new_groups.slab = 1
+	minetest.register_node(":ws_core:slab_" .. subname, {
+		description = description,
+		drawtype = "nodebox",
+		tiles = slab_images,
+		paramtype = "light",
+		paramtype2 = "facedir",
+		is_ground_content = false,
+		groups = new_groups,
+		sounds = sounds,
+		node_box = {
+			type = "fixed",
+			fixed = {-0.5, -0.5, -0.5, 0.5, 0, 0.5},
+		},
+		on_place = function(itemstack, placer, pointed_thing)
+			local under = minetest.get_node(pointed_thing.under)
+			local wield_item = itemstack:get_name()
+			local player_name = placer and placer:get_player_name() or ""
+			local creative_enabled = (creative and creative.is_enabled_for
+					and creative.is_enabled_for(player_name))
+
+			if under and under.name:find("^ws_core:slab_") then
+				-- place slab using under node orientation
+				local dir = minetest.dir_to_facedir(vector.subtract(
+					pointed_thing.above, pointed_thing.under), true)
+
+				local p2 = under.param2
+
+				-- Placing a slab on an upside down slab should make it right-side up.
+				if p2 >= 20 and dir == 8 then
+					p2 = p2 - 20
+				-- same for the opposite case: slab below normal slab
+				elseif p2 <= 3 and dir == 4 then
+					p2 = p2 + 20
+				end
+
+				-- else attempt to place node with proper param2
+				minetest.item_place_node(ItemStack(wield_item), placer, pointed_thing, p2)
+				if not creative_enabled then
+					itemstack:take_item()
+				end
+				return itemstack
+			else
+				return rotate_and_place(itemstack, placer, pointed_thing)
+			end
+		end,
+	})
+
+	-- for replace ABM
+	if replace then
+		minetest.register_node(":ws_core:slab_" .. subname .. "upside_down", {
+			replace_name = "ws_core:slab_".. subname,
+			groups = {slabs_replace = 1},
+		})
+	end
+
+	if recipeitem then
+		minetest.register_craft({
+			output = 'ws_core:slab_' .. subname .. ' 6',
+			recipe = {
+				{recipeitem, recipeitem, recipeitem},
+			},
+		})
+
+		-- Use 2 slabs to craft a full block again (1:1)
+		minetest.register_craft({
+			output = recipeitem,
+			recipe = {
+				{'ws_core:slab_' .. subname},
+				{'ws_core:slab_' .. subname},
+			},
+		})
+
+		-- Fuel
+		local baseburntime = minetest.get_craft_result({
+			method = "fuel",
+			width = 1,
+			items = {recipeitem}
+		}).time
+		if baseburntime > 0 then
+			minetest.register_craft({
+				type = "fuel",
+				recipe = 'ws_core:slab_' .. subname,
+				burntime = math.floor(baseburntime * 0.5),
+			})
+		end
+	end
+end
+
+
+-- Optionally replace old "upside_down" nodes with new param2 versions.
+-- Disabled by ws_core.
+
+if replace then
+	minetest.register_abm({
+		label = "Slab replace",
+		nodenames = {"group:slabs_replace"},
+		interval = 16,
+		chance = 1,
+		action = function(pos, node)
+			node.name = minetest.registered_nodes[node.name].replace_name
+			node.param2 = node.param2 + 20
+			if node.param2 == 21 then
+				node.param2 = 23
+			elseif node.param2 == 23 then
+				node.param2 = 21
+			end
+			minetest.set_node(pos, node)
+		end,
+	})
+end
+
+
+-- Register inner stair
+-- Node will be called ws_core:stair_inner_<subname>
+
+function ws_core.register_stair_inner(subname, recipeitem, groups, images,
+		description, sounds, worldaligntex)
+	-- Set backface culling and world-aligned textures
+	local stair_images = {}
+	for i, image in ipairs(images) do
+		if type(image) == "string" then
+			stair_images[i] = {
+				name = image,
+				backface_culling = true,
+			}
+			if worldaligntex then
+				stair_images[i].align_style = "world"
+			end
+		else
+			stair_images[i] = table.copy(image)
+			if stair_images[i].backface_culling == nil then
+				stair_images[i].backface_culling = true
+			end
+			if worldaligntex and stair_images[i].align_style == nil then
+				stair_images[i].align_style = "world"
+			end
+		end
+	end
+	local new_groups = table.copy(groups)
+	new_groups.stair = 1
+	minetest.register_node(":ws_core:stair_inner_" .. subname, {
+		description = "Inner " .. description,
+		drawtype = "nodebox",
+		tiles = stair_images,
+		paramtype = "light",
+		paramtype2 = "facedir",
+		is_ground_content = false,
+		groups = new_groups,
+		sounds = sounds,
+		node_box = {
+			type = "fixed",
+			fixed = {
+				{-0.5, -0.5, -0.5, 0.5, 0.0, 0.5},
+				{-0.5, 0.0, 0.0, 0.5, 0.5, 0.5},
+				{-0.5, 0.0, -0.5, 0.0, 0.5, 0.0},
+			},
+		},
+		on_place = function(itemstack, placer, pointed_thing)
+			if pointed_thing.type ~= "node" then
+				return itemstack
+			end
+
+			return rotate_and_place(itemstack, placer, pointed_thing)
+		end,
+	})
+
+	if recipeitem then
+		minetest.register_craft({
+			output = 'ws_core:stair_inner_' .. subname .. ' 7',
+			recipe = {
+				{"", recipeitem, ""},
+				{recipeitem, "", recipeitem},
+				{recipeitem, recipeitem, recipeitem},
+			},
+		})
+
+		-- Fuel
+		local baseburntime = minetest.get_craft_result({
+			method = "fuel",
+			width = 1,
+			items = {recipeitem}
+		}).time
+		if baseburntime > 0 then
+			minetest.register_craft({
+				type = "fuel",
+				recipe = 'ws_core:stair_inner_' .. subname,
+				burntime = math.floor(baseburntime * 0.875),
+			})
+		end
+	end
+end
+
+
+-- Register outer stair
+-- Node will be called ws_core:stair_outer_<subname>
+
+function ws_core.register_stair_outer(subname, recipeitem, groups, images,
+		description, sounds, worldaligntex)
+	-- Set backface culling and world-aligned textures
+	local stair_images = {}
+	for i, image in ipairs(images) do
+		if type(image) == "string" then
+			stair_images[i] = {
+				name = image,
+				backface_culling = true,
+			}
+			if worldaligntex then
+				stair_images[i].align_style = "world"
+			end
+		else
+			stair_images[i] = table.copy(image)
+			if stair_images[i].backface_culling == nil then
+				stair_images[i].backface_culling = true
+			end
+			if worldaligntex and stair_images[i].align_style == nil then
+				stair_images[i].align_style = "world"
+			end
+		end
+	end
+	local new_groups = table.copy(groups)
+	new_groups.stair = 1
+	minetest.register_node(":ws_core:stair_outer_" .. subname, {
+		description = "Outer " .. description,
+		drawtype = "nodebox",
+		tiles = stair_images,
+		paramtype = "light",
+		paramtype2 = "facedir",
+		is_ground_content = false,
+		groups = new_groups,
+		sounds = sounds,
+		node_box = {
+			type = "fixed",
+			fixed = {
+				{-0.5, -0.5, -0.5, 0.5, 0.0, 0.5},
+				{-0.5, 0.0, 0.0, 0.0, 0.5, 0.5},
+			},
+		},
+		on_place = function(itemstack, placer, pointed_thing)
+			if pointed_thing.type ~= "node" then
+				return itemstack
+			end
+
+			return rotate_and_place(itemstack, placer, pointed_thing)
+		end,
+	})
+
+	if recipeitem then
+		minetest.register_craft({
+			output = 'ws_core:stair_outer_' .. subname .. ' 6',
+			recipe = {
+				{"", recipeitem, ""},
+				{recipeitem, recipeitem, recipeitem},
+			},
+		})
+
+		-- Fuel
+		local baseburntime = minetest.get_craft_result({
+			method = "fuel",
+			width = 1,
+			items = {recipeitem}
+		}).time
+		if baseburntime > 0 then
+			minetest.register_craft({
+				type = "fuel",
+				recipe = 'ws_core:stair_outer_' .. subname,
+				burntime = math.floor(baseburntime * 0.625),
+			})
+		end
+	end
+end
+
+
+-- Stair/slab registration function.
+-- Nodes will be called ws_core:{stair,slab}_<subname>
+
+function ws_core.register_stair_and_slab(subname, recipeitem, groups, images,
+		desc_stair, desc_slab, sounds, worldaligntex)
+	ws_core.register_stair(subname, recipeitem, groups, images, desc_stair,
+		sounds, worldaligntex)
+	ws_core.register_stair_inner(subname, recipeitem, groups, images, desc_stair,
+		sounds, worldaligntex)
+	ws_core.register_stair_outer(subname, recipeitem, groups, images, desc_stair,
+		sounds, worldaligntex)
+	ws_core.register_slab(subname, recipeitem, groups, images, desc_slab,
+		sounds, worldaligntex)
+end
+
+
+-- Register ws_core ws_core and slabs
+
+ws_core.register_stair_and_slab(
+	"wood",
+	"ws_core:wood",
+	{choppy = 2, oddly_breakable_by_hand = 2, flammable = 2},
+	{"ws_wood.png"},
+	"Wooden Stair",
+	"Wooden Slab",
+	false
+)
+
+
+ws_core.register_stair_and_slab(
+	"cobble",
+	"default:cobble",
+	{cracky = 3},
+	{"default_cobble.png"},
+	"Cobblestone Stair",
+	"Cobblestone Slab",
+	default.node_sound_stone_defaults(),
+	true
+)
+
+ws_core.register_stair_and_slab(
+	"mossycobble",
+	"ws_core:mossycobble",
+	{cracky = 3},
+	{"ws_mossycobble.png"},
+	"Mossy Cobblestone Stair",
+	"Mossy Cobblestone Slab",
+	true
+)
+
+ws_core.register_stair_and_slab(
+	"stonebrick",
+	"ws_core:stonebrick",
+	{cracky = 2},
+	{"ws_stone_brick.png"},
+	"Stone Brick Stair",
+	"Stone Brick Slab",
+	false
+)
+
+ws_core.register_stair_and_slab(
+	"stone_block",
+	"ws_core:stone_block",
+	{cracky = 2},
+	{"ws_stone_block.png"},
+	"Stone Block Stair",
+	"Stone Block Slab",
+	true
+)
+
+ws_core.register_stair_and_slab(
+	"sandstone",
+	"default:sandstone",
+	{crumbly = 1, cracky = 3},
+	{"default_sandstone.png"},
+	"Sandstone Stair",
+	"Sandstone Slab",
+	true
+)
+
+ws_core.register_stair_and_slab(
+	"sandstonebrick",
+	"default:sandstonebrick",
+	{cracky = 2},
+	{"default_sandstone_brick.png"},
+	"Sandstone Brick Stair",
+	"Sandstone Brick Slab",
+	false
+)
+
+ws_core.register_stair_and_slab(
+	"sandstone_block",
+	"default:sandstone_block",
+	{cracky = 2},
+	{"default_sandstone_block.png"},
+	"Sandstone Block Stair",
+	"Sandstone Block Slab",
+	true
+)
+
+ws_core.register_stair_and_slab(
+	"desert_sandstone_block",
+	"default:desert_sandstone_block",
+	{cracky = 2},
+	{"default_desert_sandstone_block.png"},
+	"Desert Sandstone Block Stair",
+	"Desert Sandstone Block Slab",
+	default.node_sound_stone_defaults(),
+	true
+)
+
+ws_core.register_stair_and_slab(
+	"obsidian",
+	"ws_core:obsidian",
+	{cracky = 1, level = 2},
+	{"ws_obsidian.png"},
+	"Obsidian Stair",
+	"Obsidian Slab",
+	true
+)
+
+ws_core.register_stair_and_slab(
+	"obsidian_block",
+	"ws_core:obsidian_block",
+	{cracky = 1, level = 2},
+	{"ws_obsidian_block.png"},
+	"Obsidian Block Stair",
+	"Obsidian Block Slab",
+	true
+)
+
+ws_core.register_stair_and_slab(
+	"brick",
+	"ws_core:brick",
+	{cracky = 3},
+	{"ws_brick.png"},
+	"Brick Stair",
+	"Brick Slab",
+	false
+)
