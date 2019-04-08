3f539a5a0621b61340012e264e0f6076fec9f79c
Gerold55 <marleybeare56@gmail.com>
2019-04-08 00:35:41 -0400

    Add files via upload

--- mods/ruins/nodes.lua
+++ mods/ruins/nodes.lua
@@ -1,3 +1,4 @@
+--------------------Made by TobyPlowy-----------------------
 minetest.register_node('ruins:old_skeleton', {
 	description = 'Old Skeleton',
 	drawtype = 'mesh',
@@ -36,14 +37,6 @@ minetest.register_node('ruins:old_skeleton2', {
 	},
 })
 
-minetest.register_node('ruins:concrete', {
-	description = 'Concrete',
-	tiles = {name='ws_concrete.png'},
-	groups = {cracky=3},
-	paramtype = 'light',
-	paramtype2 = 'facedir',
-})
-
 minetest.register_node("ruins:human_skull", {
 	description = "Human Skull",
 	drawtype = "mesh",
@@ -66,4 +59,82 @@ minetest.register_node("ruins:human_skull", {
 	},
 	inventory_image = "skeleton_head_item.png",
 	groups = {choppy = 1, oddly_breakable_by_hand = 1},
+})
+
+minetest.register_node("ruins:carrot", {
+	description = "Carrot",
+	drawtype = "mesh",
+	mesh = "carrot.obj",
+	tiles = {
+		"crops.png",
+	},
+	visual_scale = 0.5,
+	paramtype = "light",
+	paramtype2 = "facedir",
+	inventory_image= "ws_carrot.png",
+	wield_image= "ws_carrot.png",
+	selection_box = {
+		type = "fixed",
+		fixed = {-0.3, -0.5, -0.3, 0.3, 0.0, 0.3}
+	},
+	collision_box = {
+		type = "fixed",
+		fixed = {-0.3, -0.5, -0.3, 0.3, 0.0, 0.3}
+	},
+	groups = {choppy = 1, oddly_breakable_by_hand = 1, flora = 1, attached_node = 1},
+})
+
+minetest.register_node("ruins:melons", {
+	description = "Melons",
+	drawtype = "mesh",
+	mesh = "melons.obj",
+	tiles = {
+		"crops.png",
+	},
+	visual_scale = 0.5,
+	paramtype = "light",
+	paramtype2 = "facedir",
+	inventory_image= "ws_melons.png",
+	wield_image= "ws_melons.png",
+	selection_box = {
+		type = "fixed",
+		fixed = {-0.3, -0.5, -0.3, 0.3, 0.0, 0.3}
+	},
+	collision_box = {
+		type = "fixed",
+		fixed = {-0.3, -0.5, -0.3, 0.3, 0.0, 0.3}
+	},
+	groups = {choppy = 1, oddly_breakable_by_hand = 1, flora = 1, attached_node = 1},
+})
+
+minetest.register_node("ruins:tomato_plant", {
+	description = "Tomato Plant",
+	drawtype = "mesh",
+	mesh = "tomato.obj",
+	tiles = {
+		"crops.png",
+	},
+	visual_scale = 0.5,
+	paramtype = "light",
+	paramtype2 = "facedir",
+	inventory_image= "ws_tomato.png",
+	wield_image= "ws_tomato.png",
+	selection_box = {
+		type = "fixed",
+		fixed = {-0.3, -0.5, -0.3, 0.3, 0.0, 0.3}
+	},
+	collision_box = {
+		type = "fixed",
+		fixed = {-0.3, -0.5, -0.3, 0.3, 0.0, 0.3}
+	},
+	groups = {choppy = 1, oddly_breakable_by_hand = 1},
+})
+------------------------
+
+minetest.register_node('ruins:concrete', {
+	description = 'Concrete',
+	tiles = {name='ws_concrete.png'},
+	groups = {cracky=3},
+	paramtype = 'light',
+	paramtype2 = 'facedir',
 })
