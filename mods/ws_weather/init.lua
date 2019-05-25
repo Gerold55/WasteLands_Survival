bda8be03ebc1d77fd20122b0a0a5c9e8e4506c06
theFox6 <the.fox6@gmx.de>
2019-05-25 13:16:07 +0200

    added acid rain

---
+++ mods/ws_weather/init.lua
@@ -0,0 +1,12 @@
+-- Acid Rain
+weather_mod.register_downfall("ws_weather:acidrain",{
+	min_pos = {x=-9, y=7, z=-9},
+	max_pos = {x= 9, y=7, z= 9},
+	falling_speed=10,
+	amount=15,
+	exptime=0.8,
+	size=25,
+	texture="ws_weather_acidrain.png",
+	enable_lightning=true,
+	damage_player={amount = 1, chance = .8, steps = 80},
+})
