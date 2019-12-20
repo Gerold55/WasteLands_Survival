2ec6c6a648a267700e8084fce3512ed4d6302922
theFox6 <the.fox6@gmx.de>
2019-12-20 15:23:06 +0100

    fix ruin generation

--- mods/ruins/structures.lua
+++ mods/ruins/structures.lua
@@ -4,44 +4,49 @@ local mg_name = minetest.get_mapgen_setting("mg_name")
 -- internationalization boilerplate
 local S = minetest.get_translator(minetest.get_current_modname())
 
-minetest.register_decoration({
-		name = "ruins:ruin",
-		deco_type = "schematic",
-		place_on = {"ws_core:dirt_dry"},
-		sidelen = 1,
-		noise_params = {
-			offset = 0.0001,
-			scale = 0.00004,
-			spread = {x = 200000, y = 200000, z = 200000},
-			seed = 2,
-			octaves = 3,
-			persist = 5
-		},
-		biomes = {"dirtland"},
-		y_min = 2,
-		y_max = 80,
-		schematic = modpath.."/schematics/ruin1.mts",
-		flags = "place_center_x, place_center_z,  force_placement",
-		rotation = "random",
-	})
-	
-	minetest.register_decoration({
-		name = "ruins:ruin1",
-		deco_type = "schematic",
-		place_on = {"ws_core:sandy_dirt"},
-		sidelen = 2,
-		noise_params = {
-			offset = 0.0001,
-			scale = 0.00004,
-			spread = {x = 300000, y = 300000, z = 300000},
-			seed = 2,
-			octaves = 3,
-			persist = 0.66
-		},
-		biomes = {"desert"},
-		y_min = 4,
-		y_max = 5,
-		schematic = modpath.."/schematics/ruin1_sand.mts",
-		flags = "place_center_x, place_center_z,  force_placement",
-		rotation = "random",
-	})
+ruins.decorationIDs = {}
+
+local r
+r = minetest.register_decoration({
+	name = "ruins:ruin",
+	deco_type = "schematic",
+	place_on = {"ws_core:dirt_dry"},
+	sidelen = 36,
+	noise_params = {
+		offset = 0.0001,
+		scale = 0.003,
+		spread = {x = 100, y = 100, z = 100},
+		seed = 2,
+		octaves = 3,
+		persist = 0.6
+	},
+	biomes = {"dirtland","forest"},
+	y_min = 2,
+	y_max = 80,
+	schematic = modpath.."/schematics/ruin1.mts",
+	flags = "place_center_x, place_center_z,  force_placement",
+	rotation = "random",
+})
+table.insert(ruins.decorationIDs,r)
+
+r = minetest.register_decoration({
+	name = "ruins:ruin1",
+	deco_type = "schematic",
+	place_on = {"ws_core:sandy_dirt"},
+	sidelen = 46,
+	noise_params = {
+		offset = 0.0001,
+		scale = 0.001,
+		spread = {x = 100, y = 100, z = 100},
+		seed = 2,
+		octaves = 3,
+		persist = 0.6
+	},
+	biomes = {"desert"},
+	y_min = 2,
+	y_max = 6,
+	schematic = modpath.."/schematics/ruin1_sand.mts",
+	flags = "place_center_x, place_center_z,  force_placement",
+	rotation = "random",
+})
+table.insert(ruins.decorationIDs,r)
