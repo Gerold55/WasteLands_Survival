d1ee9f0b2ddb8279b8e12b84a1357ed9b13d757c
theFox6 <the.fox6@gmx.de>
2019-12-21 10:23:15 +0100

    added more types of ruins

--- mods/ruins/structures.lua
+++ mods/ruins/structures.lua
@@ -14,7 +14,7 @@ r = minetest.register_decoration({
 	sidelen = 36,
 	noise_params = {
 		offset = 0.0001,
-		scale = 0.003,
+		scale = 0.0008,
 		spread = {x = 100, y = 100, z = 100},
 		seed = 2,
 		octaves = 3,
@@ -44,9 +44,75 @@ r = minetest.register_decoration({
 	},
 	biomes = {"desert"},
 	y_min = 2,
-	y_max = 6,
+	y_max = 16,
 	schematic = modpath.."/schematics/ruin1_sand.mts",
 	flags = "place_center_x, place_center_z,  force_placement",
+	rotation = "random",
+})
+table.insert(ruins.decorationIDs,r)
+
+r = minetest.register_decoration({
+	name = "ruins:ruin_large",
+	deco_type = "schematic",
+	place_on = {"ws_core:dirt_dry"},
+	sidelen = 80,
+	noise_params = {
+		offset = 0.00004,
+		scale = 0.0004,
+		spread = {x = 100, y = 100, z = 100},
+		seed = 8,
+		octaves = 2,
+		persist = 0.8
+	},
+	biomes = {"dirtland","forest"},
+	y_min = 2,
+	y_max = 80,
+	schematic = modpath.."/schematics/ruin_large.mts",
+	flags = "place_center_x, place_center_z",
+	rotation = "random",
+})
+table.insert(ruins.decorationIDs,r)
+
+r = minetest.register_decoration({
+	name = "ruins:ruin_large_sand",
+	deco_type = "schematic",
+	place_on = {"ws_core:sandy_dirt"},
+	sidelen = 80,
+	noise_params = {
+		offset = 0.00004,
+		scale = 0.0008,
+		spread = {x = 100, y = 100, z = 100},
+		seed = 8,
+		octaves = 2,
+		persist = 0.8
+	},
+	biomes = {"desert"},
+	y_min = 2,
+	y_max = 80,
+	schematic = modpath.."/schematics/ruin_large_sand.mts",
+	flags = "place_center_x, place_center_z",
+	rotation = "random",
+})
+table.insert(ruins.decorationIDs,r)
+
+r = minetest.register_decoration({
+	name = "ruins:ramshackle_shelter",
+	deco_type = "schematic",
+	place_on = {"ws_core:dirt_dry"},
+	sidelen = 80,
+	noise_params = {
+		offset = 0.00004,
+		scale = 0.0001,
+		spread = {x = 100, y = 100, z = 100},
+		seed = 12,
+		octaves = 2,
+		persist = 0.2
+	},
+	biomes = {"dirtland","forest"},
+	y_min = 2,
+	y_max = 80,
+	schematic = modpath.."/schematics/ramshackle_shelter.mts",
+	flags = "place_center_x, place_center_z, force_placement",
 	rotation = "random",
 })
 table.insert(ruins.decorationIDs,r)
