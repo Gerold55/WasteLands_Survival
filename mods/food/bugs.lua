0e347fa8c43dda1e3734cc66cb40bd81309f51a0
Gerold55 <marleybeare56@gmail.com>
2020-04-15 20:34:52 -0400

    Make Bugs Eatable

--- mods/food/bugs.lua
+++ mods/food/bugs.lua
@@ -2,28 +2,33 @@ minetest.register_craftitem("food:bug_1", {
 	description = "Beetle",
 	inventory_image = "food_beetle.png",
 	groups = {food_bugs = 1, flammable = 1},
+	on_use = minetest.item_eat(1),
 })
 
 minetest.register_craftitem("food:bug_2", {
 	description = "Worm",
 	inventory_image = "food_worm.png",
 	groups = {food_bugs = 1, flammable = 1},
+	on_use = minetest.item_eat(1),
 })
 
 minetest.register_craftitem("food:bug_3", {
 	description = "Cockroach",
 	inventory_image = "food_cockroach.png",
 	groups = {food_bugs = 1, flammable = 1},
+	on_use = minetest.item_eat(1),
 })
 
 minetest.register_craftitem("food:bug_4", {
 	description = "Larva",
 	inventory_image = "food_larva.png",
 	groups = {food_bugs = 1, flammable = 1},
+	on_use = minetest.item_eat(1),
 })
 
 minetest.register_craftitem("food:bug_5", {
 	description = "Moth",
 	inventory_image = "food_moth.png",
 	groups = {food_bugs = 1, flammable = 1},
+	on_use = minetest.item_eat(1),
 })
