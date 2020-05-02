4996f7f193d3ab33cb0e17719e4fa38d21674b44
theFox6 <the.fox6@gmx.de>
2020-05-02 14:01:15 +0200

    added rusty girder

--- mods/derilict_building/girder.lua
+++ mods/derilict_building/girder.lua
@@ -23,4 +23,23 @@ minetest.register_node("derilict_building:girder", {
   sounds = ws_core and ws_core.node_sound_metal_defaults(),
 })
 
+minetest.register_node("derilict_building:girder_rusty", {
+  description = "rusty steel girder",
+  drawtype = "nodebox",
+  selection_box = {
+    type = "fixed",
+    fixed = girder_nodebox,
+  },
+  node_box = {
+    type = "fixed",
+    fixed = girder_nodebox,
+  },
+  paramtype = "light",
+  paramtype2 = "facedir",
+  groups = {cracky=1, level = 2},
+  tiles = {"derilict_building_steel_red_rusty.png"},
+  is_ground_content = false,
+  sounds = ws_core and ws_core.node_sound_metal_defaults(),
+})
+
 --TODO: craft t-shape out of bars
