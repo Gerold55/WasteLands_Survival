c94eb60a6919092436f3a452a9ae471aeca5bdbe
Skamiz <48406239+Skamiz@users.noreply.github.com>
2020-09-10 08:58:04 +0200

    change loot
    add food cans
    make clay pots work as intended instead of crashing the game
    rebalance loot

--- mods/ws_core/storage.lua
+++ mods/ws_core/storage.lua
@@ -30,76 +30,70 @@ minetest.register_node("ws_core:barrel", {
 minetest.register_node("ws_core:clay_pot", {
 	description = "Clay Pot",
 	drawtype = "nodebox",
-	paramtype2 = "facedir",
+	paramtype = "light",
 	tiles = {
 		"ws_clay_pot_top_closed.png",
 		"ws_clay_pot_bottom.png",
 		"ws_clay_pot.png",
-		"ws_clay_pot.png",
-		"ws_clay_pot.png",
-		"ws_clay_pot.png"
 	},
 	is_ground_content = false,
-	groups = {choppy = 2, oddly_breakable_by_hand = 2},
+	groups = {oddly_breakable_by_hand = 2},
+	node_box = {
+		type = "fixed",
+		fixed = {
+			{-0.375, -0.5, -0.375, 0.375, -0.4375, 0.375}, -- NodeBox1
+			{-0.4375, -0.4375, -0.4375, 0.4375, 0.3125, 0.4375}, -- NodeBox2
+			{-0.375, 0.3125, -0.375, 0.375, 0.375, 0.375}, -- NodeBox3
+			{-0.4375, 0.375, -0.4375, 0.4375, 0.5, 0.4375}, -- NodeBox4
+		}
+	},
 	on_punch = function(pos, node, clicker, itemstack)
-    local meta = minetest.env:get_meta(pos)
-	node.name = "ws_core:clay_pot_opened"
-		minetest.set_node(pos, node)
+		local meta = minetest.get_meta(pos)
+		meta:set_string('formspec',
+				'size [8,8]'..
+				'bgcolor[#080808BB;false]'..
+				'list[context;main;0,0.2;8,3;]'..
+				'list[current_player;main;0,4;8,4;]'..
+				'listring[]')
+		node.name = "ws_core:clay_pot_opened"
+		minetest.swap_node(pos, node)
 	end,
-	node_box = {
-		type = "fixed",
-		fixed = {
-			{-0.375, -0.5, -0.375, 0.375, -0.4375, 0.375}, -- NodeBox1
-			{-0.4375, -0.4375, -0.4375, 0.4375, 0.3125, 0.4375}, -- NodeBox2
-			{-0.375, 0.3125, -0.375, 0.375, 0.375, 0.375}, -- NodeBox3
-			{-0.4375, 0.375, -0.4375, 0.4375, 0.5, 0.4375}, -- NodeBox4
-		}
-	}
-})
-
-minetest.register_node("ws_core:clay_pot_opened", {
-	description = "Clay Pot",
-	drawtype = "nodebox",
-	paramtype2 = "facedir",
-	tiles = {
-		"ws_clay_pot_top.png",
-		"ws_clay_pot_bottom.png",
-		"ws_clay_pot.png",
-		"ws_clay_pot.png",
-		"ws_clay_pot.png",
-		"ws_clay_pot.png"
-	},
-	is_ground_content = false,
-	groups = {choppy = 2, oddly_breakable_by_hand = 2, flammable = 2, not_in_creative_inventory = 1},
-	node_box = {
-		type = "fixed",
-		fixed = {
-			{-0.375, -0.5, -0.375, 0.375, -0.4375, 0.375}, -- NodeBox1
-			{-0.4375, -0.4375, -0.4375, 0.4375, 0.3125, 0.4375}, -- NodeBox2
-			{-0.375, 0.3125, -0.375, 0.375, 0.375, 0.375}, -- NodeBox3
-			{-0.4375, 0.375, -0.4375, 0.4375, 0.5, 0.4375}, -- NodeBox4
-		}
-	},
 	on_construct = function(pos)
 		local meta = minetest.get_meta(pos)
 		local inv = meta:get_inventory()
 		inv:set_size('main', 8*4)
-		inv:set_size('storage', 9*3)
-		meta:set_string('formspec',
-			'size [9,9]'..
-			'bgcolor[#080808BB;false]'..
-			'list[current_name;storage;0,0.2;9,3;]'..
-			'list[current_player;main;0.5,5;8,4;]')
 	end,
+})
+
+minetest.register_node("ws_core:clay_pot_opened", {
+	description = "Clay Pot Open",
+	drawtype = "nodebox",
+	paramtype = "light",
+	tiles = {
+		"ws_clay_pot_top.png",
+		"ws_clay_pot_bottom.png",
+		"ws_clay_pot.png",
+	},
+	is_ground_content = false,
+	groups = {oddly_breakable_by_hand = 2, not_in_creative_inventory = 1},
+	node_box = {
+		type = "fixed",
+		fixed = {
+			{-0.375, -0.5, -0.375, 0.375, -0.4375, 0.375}, -- NodeBox1
+			{-0.4375, -0.4375, -0.4375, 0.4375, 0.3125, 0.4375}, -- NodeBox2
+			{-0.375, 0.3125, -0.375, 0.375, 0.375, 0.375}, -- NodeBox3
+			{-0.4375, 0.375, -0.4375, 0.4375, 0.5, 0.4375}, -- NodeBox4
+		}
+	},
 	on_punch = function(pos, node, clicker, itemstack)
-    local meta = minetest.env:get_meta(pos)
-	node.name = "ws_core:clay_pot"
-		minetest.set_node(pos, node)
+		local meta = minetest.get_meta(pos)
+		meta:set_string('formspec', "")
+		node.name = "ws_core:clay_pot"
+		minetest.swap_node(pos, node)
 	end,
 	can_dig = function(pos,player)
 		local meta = minetest.get_meta(pos);
 		local inv = meta:get_inventory()
-		return inv:is_empty('storage')
+		return inv:is_empty('main')
 	end,
-	on_place = minetest.rotate_node
 })
